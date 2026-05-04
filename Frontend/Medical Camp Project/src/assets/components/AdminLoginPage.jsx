84f6f847c4d2a4d42d719e4af40173b4c0dca1dd
Neeraj2266 <neerajgangavarapu@gmail.com>
2026-05-04 13:05:59 +0530

    tested the working of tailwind

---
+++ Frontend/Medical Camp Project/src/assets/components/AdminLoginPage.jsx
@@ -0,0 +1,20 @@
+
+import React from "react"
+
+function AdminLoginPage() {
+    return (
+
+        <div className="flex items-center justify-center h-screen bg-black">
+            <h1 className="text-red-500 text-4xl font-bold">
+                Tailwind Working 🚀
+            </h1>
+        </div>
+    )
+}
+
+export default AdminLoginPage;
+
+
+
+
+
